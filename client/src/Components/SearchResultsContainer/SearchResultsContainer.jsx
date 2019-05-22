ce737354607296ca172932ce90c56f1cff7c284a
Dylan Davidson <rogue1037@gmail.com>
2019-05-22 13:50:17 -0700

    Adding small changes

--- client/src/Components/SearchResultsContainer/SearchResultsContainer.jsx
+++ client/src/Components/SearchResultsContainer/SearchResultsContainer.jsx
@@ -20,7 +20,7 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   heroContent: {
-    maxWidth: 600,
+    maxWidth: 500,
     margin: '0 auto',
     padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
   },
