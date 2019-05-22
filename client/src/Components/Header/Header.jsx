930d5c39f4393dabd7d9781bb3483f44238455cd
Johnathan <j.vargas2319@gmail.com>
2019-05-22 11:02:24 -0700

    added header img

---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import headerCss from './HeaderCss'
+
+function header(){
+    return (
+     
+            <header>
+                <img src="/assets/img/5m.gif" alt="" />
+            </header>
+       
+    )
+}
+
+export default (header);
