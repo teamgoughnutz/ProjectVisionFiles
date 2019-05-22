26b3f30bfbac3dba9631615ed21c6e6ee4aec67a
Cris <cherrera22@gmail.com>
2019-05-22 10:54:34 -0700

    app bar install

--- client/src/Components/CharacterSearch/CharacterSearch.jsx
+++ client/src/Components/CharacterSearch/CharacterSearch.jsx
@@ -1,67 +1,182 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
-
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 const styles = theme => ({
-  container: {
+  root: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
     display: 'flex',
-    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-   
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
   },
-  dense: {
-    marginTop: 16,
-  },
-  menu: {
-    width: 200,
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
   },
 });
 
-
-
-function CharacterSearch(props) {
+function SearchAppBar(props) {
   const { classes } = props;
   return (
-    <div className={classes.heroButtons}>
-      <Grid container spacing={8} justify="center">
-        <div className={classes.root}>
-          <FormControl className={classes.margin}>
-            <InputLabel
-              htmlFor="custom-css-standard-input"
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+            Material-UI
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
               classes={{
-                root: classes.cssLabel,
-                focused: classes.cssFocused,
+                root: classes.inputRoot,
+                input: classes.inputInput,
               }}
-              onChange={props.handleInputChange}
-              value={props.search}
-              name="search"
-              type="text"
-              placeholder="Vision Files"
-              id="search"
             />
-            <Grid item>
-              <Button onClick={props.handleFormSubmit} variant="contained" color="primary">
-                Execute
-                  </Button>
-            </Grid>
-          </FormControl>
-
-        </div>
-      </Grid>
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
+  );
 }
 
-CharacterSearch.propTypes = {
+SearchAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(CharacterSearch);
+export default withStyles(styles)(SearchAppBar);
+
+
+
+/////
+/////
+/////
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Button from '@material-ui/core/Button';
+// import FormControl from '@material-ui/core/FormControl';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import Grid from '@material-ui/core/Grid';
+// import { withStyles } from '@material-ui/core/styles';
+
+
+// const styles = theme => ({
+//   container: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//   },
+//   textField: {
+//     marginLeft: theme.spacing.unit,
+//     marginRight: theme.spacing.unit,
+   
+//   },
+//   dense: {
+//     marginTop: 16,
+//   },
+//   menu: {
+//     width: 200,
+//   },
+// });
+
+
+
+// function CharacterSearch(props) {
+//   const { classes } = props;
+//   return (
+//     <div className={classes.heroButtons}>
+//       <Grid container spacing={8} justify="center">
+//         <div className={classes.root}>
+//           <FormControl className={classes.margin}>
+//             <InputLabel
+//               htmlFor="custom-css-standard-input"
+//               classes={{
+//                 root: classes.cssLabel,
+//                 focused: classes.cssFocused,
+//               }}
+//               onChange={props.handleInputChange}
+//               value={props.search}
+//               name="search"
+//               type="text"
+//               placeholder="Vision Files"
+//               id="search"
+//             />
+//             <Grid item>
+//               <Button onClick={props.handleFormSubmit} variant="contained" color="primary">
+//                 Execute
+//                   </Button>
+//             </Grid>
+//           </FormControl>
+
+//         </div>
+//       </Grid>
+//     </div>
+//   )
+// }
+
+// CharacterSearch.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(CharacterSearch);
